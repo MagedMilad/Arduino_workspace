04767d2febb8e470b435ea8fc92f8bcbea43a075
magedmilad <magedmilad08@gmail.com>
2015-10-06 18:31:20 +0200

    maged edit version 1.0

---
+++ Morse_Code/Morse_Code.ino
@@ -0,0 +1,46 @@
+const int ledPin = 13;
+
+void setup() {
+ pinMode(ledPin,OUTPUT);
+
+}
+
+void loop() {
+  digitalWrite(lenPin , HIGH);
+  dalay(1000);
+  digitalWrite(lenPin , LOW);
+  dalay(1000);
+  digitalWrite(lenPin , HIGH);
+  dalay(1000);
+  digitalWrite(lenPin , LOW);
+  dalay(1000);
+  digitalWrite(lenPin , HIGH);
+  dalay(1000);
+  digitalWrite(lenPin , LOW);
+  dalay(3000);
+  digitalWrite(lenPin , HIGH);
+  dalay(3000);
+  digitalWrite(lenPin , LOW);
+  dalay(1000);
+  digitalWrite(lenPin , HIGH);
+  dalay(3000);
+  digitalWrite(lenPin , LOW);
+  dalay(1000);
+  digitalWrite(lenPin , HIGH);
+  dalay(3000);
+  digitalWrite(lenPin , LOW);
+  dalay(3000);
+  digitalWrite(lenPin , HIGH);
+  dalay(1000);
+  digitalWrite(lenPin , LOW);
+  dalay(1000);
+  digitalWrite(lenPin , HIGH);
+  dalay(1000);
+  digitalWrite(lenPin , LOW);
+  dalay(1000);
+  digitalWrite(lenPin , HIGH);
+  dalay(1000);
+  digitalWrite(lenPin , LOW);
+  dalay(7000);
+}
+
