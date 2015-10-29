21c49dd1519fd1f44caebc0c1ec67b09fcbc2406
mina sameh labib <mena_monster@yahoo.com>
2015-10-29 19:29:04 +0200

    added comments

--- Voltage_threshold_detector/Voltage_threshold_detector.ino
+++ Voltage_threshold_detector/Voltage_threshold_detector.ino
@@ -1,5 +1,12 @@
+/*
+ * function : trigger a warning sound and light if the voltage on 
+ *            pot_pin exceeds the threshold voltage
+ * input    : potentiometer  
+ * output   : a led and a buzzer
+ */
 
-const int pot_pin = 0; // choose one ya mina
+
+const int pot_pin = 14;
 const int out_led = 13;
 
 void setup() {
@@ -8,13 +15,15 @@ void setup() {
 }
 
 void loop() {
- int val = analogRead(pot_pin);
- float volt  = (val / 1023.0) * 5.0;
- Serial.println(volt);
- if(volt > 3){
-  digitalWrite(out_led,HIGH);
- }
- else if(volt < 2.5){
-  digitalWrite(out_led,LOW);
- }
+  //read the current voltage at 10 bit resolution (0 to 1023)
+  int val = analogRead(pot_pin);
+  //scale the value to volt (0 to 5)
+  float volt  = (val / 1023.0) * 5.0;
+  Serial.println(volt);
+  if(volt > 3){
+    digitalWrite(out_led,HIGH);
+  }
+  else if(volt < 2.5){
+    digitalWrite(out_led,LOW);
+  }
 }
